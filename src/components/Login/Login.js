ec752e91e82c4852ab17ffa28c083f368654987c
Katie Wilday <wildayk@gmail.com>
2020-05-09 22:54:39 -0400

    image adjustment

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import loginImg from "../../login.svg";
 
-export class Login extends React.Component {
+export default class Login extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -9,11 +9,11 @@ export class Login extends React.Component {
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Login</div>
         <div className="content">
           <div className="image">
             <img src={loginImg} />
           </div>
+          <div className="login-header">Login</div>
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,45 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import loginImg from "../../login.svg";
 
-export class Register extends React.Component {
+export default class Registration extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      registrationErrors: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    const { email, password, password_confirmation } = this.state;
+
+    axios
+      .post(
+        "http://localhost:3001/register",
+        {
+          user: {
+            email: email,
+            password: password,
+            password_confirmation: password_confirmation
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth(response.data);
+        }
+      })
+      .catch(error => {
+        console.log("registration error", error);
+      });
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Register</div>
-        <div className="content">
-          <div className="image">
-            <img src={loginImg} />
-          </div>
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text"
-                     name="username"
-                     placeholder="username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text"
-                     name="email"
-                     placeholder="email" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text"
-                     name="password"
-                     placeholder="password" />
-            </div>
-          </div>
-        </div>
-        <div className="footer">
-          <button type="button" className="btn">
-            Register
-          </button>
-        </div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Password confirmation"
+            value={this.state.password_confirmation}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type="submit">Register</button>
+        </form>
       </div>
     );
   }
