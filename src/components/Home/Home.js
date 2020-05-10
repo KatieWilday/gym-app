f29ffb7a177326c4cdb822bcb0a4be0a44720f57
Katie Wilday <wildayk@gmail.com>
2020-05-09 22:38:20 -0400

    login/register

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Login from '../Login/Login'
 
 class Home extends React.Component {
    render() {
       return (
          <div>
             <h1>Home...</h1>
+            <Login />
          </div>
       )
    }
